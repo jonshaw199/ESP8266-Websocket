9d2d32077af073cba0bd34d74854d433802d4ee3
Jon <jonshaw199@gmail.com>
2022-08-25 14:08:16 -0700

    Build flags; echo server url/path; prettier

--- examples/WebSocketClient_Demo/WebSocketClient_Demo.ino
+++ examples/WebSocketClient_Demo/WebSocketClient_Demo.ino
@@ -1,18 +1,21 @@
 #include <ESP8266WiFi.h>
 #include <WebSocketClient.h>
 
-const char* ssid     = "SSID HERE";
-const char* password = "PASSWORD HERE";
-char path[] = "/";
-char host[] = "echo.websocket.org";
-  
+#include "pre.h"
+
+const char *ssid = STRINGIFY(JSSSID);
+const char *password = STRINGIFY(JSPASS);
+char path[] = "/raw";
+char host[] = "ws.postman-echo.com";
+
 WebSocketClient webSocketClient;
 
 // Use WiFiClient class to create TCP connections
 WiFiClient client;
 
-void setup() {
-  Serial.begin(115200);
+void setup()
+{
+  Serial.begin(JS_BAUD);
   delay(10);
 
   // We start by connecting to a WiFi network
@@ -21,28 +24,32 @@ void setup() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
+
   WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
 
   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
   delay(5000);
-  
 
   // Connect to the websocket server
-  if (client.connect("echo.websocket.org", 80)) {
+  if (client.connect("echo.websocket.org", 80))
+  {
     Serial.println("Connected");
-  } else {
+  }
+  else
+  {
     Serial.println("Connection failed.");
-    while(1) {
+    while (1)
+    {
       // Hang on failure
     }
   }
@@ -50,43 +57,49 @@ void setup() {
   // Handshake with the server
   webSocketClient.path = path;
   webSocketClient.host = host;
-  if (webSocketClient.handshake(client)) {
+  if (webSocketClient.handshake(client))
+  {
     Serial.println("Handshake successful");
-  } else {
-    Serial.println("Handshake failed.");
-    while(1) {
-      // Hang on failure
-    }  
   }
-
+  else
+  {
+    Serial.println("Handshake failed.");
+    while (1)
+    {
+      // Hang on failure
+    }
+  }
 }
 
-
-void loop() {
+void loop()
+{
   String data;
 
-  if (client.connected()) {
-    
+  if (client.connected())
+  {
+
     webSocketClient.getData(data);
-    if (data.length() > 0) {
+    if (data.length() > 0)
+    {
       Serial.print("Received data: ");
       Serial.println(data);
     }
-    
+
     // capture the value of analog 1, send it along
     pinMode(1, INPUT);
     data = String(analogRead(1));
-    
+
     webSocketClient.sendData(data);
-    
-  } else {
+  }
+  else
+  {
     Serial.println("Client disconnected.");
-    while (1) {
+    while (1)
+    {
       // Hang on disconnect.
     }
   }
-  
+
   // wait to fully let the client disconnect
   delay(3000);
-  
 }
---
+++ pre.h
@@ -0,0 +1,7 @@
+
+#ifndef JS_BAUD
+#define JS_BAUD 115200
+#endif
+
+#define STRINGIFY(s) STRINGIFY1(s)
+#define STRINGIFY1(s) #s
